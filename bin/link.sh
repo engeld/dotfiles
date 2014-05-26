008d47bf6cb4ae07ef06dbcd4121ef182b8c019a
Dana Engel <engel.dana.mail@gmail.com>
2014-05-26 15:28:11 +0200

    checks now if one of the configfiles is a directory and won't link it

--- bin/link.sh
+++ bin/link.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #set -x
+set -o nounset
+set -o errexit
+
 
 CONF_PATH=`dirname $0`"/../conf/*"
 for FILE in `echo ${CONF_PATH}`; do
-  echo "Linking file ${FILE} to ${HOME}/.`basename ${FILE}`"
+  if [ -d ${FILE} ]; then
+    echo "${FILE} is a directory, not gonna link this one"
+  else
+    echo "Linking file ${FILE} to ${HOME}/.`basename ${FILE}`"
+  fi
 done;
