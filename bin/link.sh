cd383bf37ae1a35cb3a33c3080dde3b249eeb254
Dana Engel <engel.dana.mail@gmail.com>
2014-05-27 10:39:32 +0200

    add actual linking to bin/link.sh and add a check if there's maybe already a file at the link target

--- bin/link.sh
+++ bin/link.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 #set -x
-set -o nounset
-set -o errexit
 
+#FIXME: add handling for directories
+#TODO: add checks after linking to check if all created links are valid
 
-CONF_PATH=`dirname $0`"/../conf/*"
+CONF_PATH="${HOME}/.dotfiles/conf/*"
 for FILE in `echo ${CONF_PATH}`; do
   if [ -d ${FILE} ]; then
     echo "${FILE} is a directory, not gonna link this one"
   else
-    echo "Linking file ${FILE} to ${HOME}/.`basename ${FILE}`"
+    SYMLINK_TARGET="${HOME}/.`basename ${FILE}`"
+    
+    if [ -e ${SYMLINK_TARGET} ]; then
+      echo "ERROR: could not link ${FILE} to ${SYMLINK_TARGET}. ${SYMLINK_TARGET} already exists"
+    else
+      echo "Linking file ${FILE} to ${SYMLINK_TARGET}"
+      ln -s ${FILE} ${SYMLINK_TARGET}
+    fi
+   
   fi
 done;
